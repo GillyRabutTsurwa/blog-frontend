de4b089656fb0ceb34fa8a91c44517a6011aa40e
Gilbert Tsurwa <tsurwagilbert@gmail.com>
2023-01-25 00:12:53 -0500

    Tech & Personal Posts Pages - created and started developing the pages that will display all the posts

---
+++ pages/tech/posts/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <h4>Tech Posts</h4>
+    <div v-for="(currentPost) in state.posts" :key="currentPost._id">
+      <NuxtLink :to="`/tech/posts/${currentPost.slug.current}`">
+        <SanityImage :asset-id="currentPost.mainImage.asset._ref" auto="format" />
+        <h4>{{ currentPost.title }}</h4>
+      </NuxtLink>
+    </div>
+  </div>
+</template>
+  
+  
+<script setup>
+const state = reactive({
+  posts: [],
+});
+
+const query = `*[_type == "tech-post"]`;
+const { data, error } = await useSanityQuery(query);
+state.posts = data.value;
+console.log(state.posts);
+
+//TODO: put this into a composable. as I'm using it elsewhere
+const formatDate = (currentDate) => {
+  const options = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+  return new Date(currentDate).toLocaleDateString("en-GB", options);
+};
+</script>
+  
+  <style>
+/* TESTING */
+img {
+  width: 5rem;
+  height: 5rem;
+}
+</style>
