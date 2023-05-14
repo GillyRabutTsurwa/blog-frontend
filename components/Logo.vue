6ab0d1e617a68e2015a68a87b4b3641e0e5d047c
Gilbert Tsurwa <tsurwagilbert@gmail.com>
2023-05-14 13:23:59 -0400

    Fixed Small Bug in Header Pertaining to Logo

--- components/Logo.vue
+++ components/Logo.vue
@@ -1,17 +1,5 @@
-<script setup>
-const route = useRoute();
-console.log(route);
-console.log(route.fullPath);
-
-//NOTE: this code works great but i don't have a nice looking header. will comment it out until then
-const isHeaderPlaced = computed(() => {
-  // NOTE: locations that I do NOT want the Header to show
-  return route.fullPath !== "/" && route.name !== "tech" && route.name !== "personal-posts-slug" && route.name !== "authentication";
-});
-</script>
-
 <template>
-  <figure v-if="isHeaderPlaced">
+  <figure>
     <NuxtLink to="/">
       <img src="../assets/img/svg/my-logo.svg" alt="">
     </NuxtLink>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,4 +1,11 @@
+<script setup>
+const route = useRoute();
+console.log(route.name);
+console.log(route.path);
+const hideNav = route.name === "index" && route.name === "personal-posts-slug";
+</script>
+
 <template>
-  <Navigation v-if="$route.path !== '/'" />
+  <Navigation v-if="$route.name !== 'index' && $route.name !== 'personal-posts-slug'" />
   <slot />
 </template>
