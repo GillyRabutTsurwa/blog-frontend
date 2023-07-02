d47f0f35656684305cd904495fca779d18c89578
Gilbert Tsurwa <tsurwagilbert@gmail.com>
2023-07-01 20:04:20 -0400

    Added .env Value for Production - i must do this because .env is ignored - so unless i make two .env files (which i may do later), things will not work the way i set it up

--- sanity.client.vite.js
+++ sanity.client.vite.js
@@ -3,7 +3,7 @@ import { createClient } from "@sanity/client";
 export const client = createClient({
     name: "blog",
     projectId: import.meta.env.VITE_SANITY_PROJECT_ID,
-    dataset: import.meta.env.VITE_SANITY_ENV,
+    dataset: import.meta.env.VITE_SANITY_ENV_STAGE,
     apiVersion: import.meta.env.VITE_SANITY_API_VERSION,
     useCdn: false,
 });
